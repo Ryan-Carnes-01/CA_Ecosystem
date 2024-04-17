caadbaf60bfc0d848519c2055730cb4b9670231e
Ryan-Carnes-01 <rcarnes@vols.utk.edu>
2024-04-17 17:18:29 -0400

    Added flag to avoid repeated movement of cells

--- ecosystem.py
+++ ecosystem.py
@@ -8,14 +8,14 @@ class Prey:
         self.reproduce = False
         self.value = 1
         self.location = (0,0)
-        self.previous_location = (0,0)
+        self.move = True
 
 class Predator:
     def __init__(self):
         self.reproduce = False
         self.value = 2
         self.location = (0,0)
-        self.previous_location = (0,0)
+        self.move = True
 
 def move_up(board,cell):
     cell_row,cell_col = cell.location
@@ -23,6 +23,7 @@ def move_up(board,cell):
     board[cell_row-1][cell_col] = cell
     board[cell_row][cell_col] = None
     cell.location = (cell_row-1, cell_col)
+    cell.move = False
 
 def move_down(board,cell):
     cell_row,cell_col = cell.location
@@ -30,7 +31,7 @@ def move_down(board,cell):
     board[cell_row+1][cell_col] = cell
     board[cell_row][cell_col] = None
     cell.location = (cell_row+1, cell_col)
-
+    cell.move = False
 
 def move_right(board,cell):
     cell_row,cell_col = cell.location
@@ -38,6 +39,7 @@ def move_right(board,cell):
     board[cell_row][cell_col+1] = cell
     board[cell_row][cell_col] = None
     cell.location = (cell_row, cell_col+1)
+    cell.move = False
 
 def move_left(board,cell):
     cell_row,cell_col = cell.location
@@ -45,6 +47,7 @@ def move_left(board,cell):
     board[cell_row][cell_col-1] = cell
     board[cell_row][cell_col] = None
     cell.location = (cell_row, cell_col-1)
+    cell.move = False
 
 def move_random(board,cell):
     random_number = random.randint(0, 3)
@@ -66,10 +69,8 @@ def populate_board(board, prey_density, predator_density):
     print("Populating board...\n")
     num_rows = len(board)
     num_cols = len(board[0])
-
     num_prey = int(num_rows * num_cols * prey_density)
     num_predators = int(num_rows * num_cols * predator_density)
-
     #Populate prey
     for _ in range(num_prey):
         row = random.randint(0, num_rows - 1)
@@ -77,8 +78,6 @@ def populate_board(board, prey_density, predator_density):
         if board[row][col] is None:
             board[row][col] = Prey()
             board[row][col].location = (row, col)
-            board[row][col].previous_location = (row,col)
-
     #Populate predators
     for _ in range(num_predators):
         row = random.randint(0, num_rows - 1)
@@ -86,7 +85,6 @@ def populate_board(board, prey_density, predator_density):
         if board[row][col] is None:
             board[row][col] = Predator()
             board[row][col].location = (row, col)
-            board[row][col].previous_location = (row,col)
     return
 
 def visualize_board(board):
@@ -181,16 +179,22 @@ def update_cell(board,cell,option):
     cell_row,cell_col = cell.location
     option_str = ''.join(map(str, option))
     option_int = int(option_str)
-    if '9' in option_str:
-        return #(IGNORE BORDER CASES FOR NOW)
     print(option_int)
+    #if cell has already moved, dont move it
+    if cell.move == False:
+        print("CELL ALREADY MOVED, RETURNING")
+        return
     if(isinstance(cell, Predator)):
         #update cell based on option
+        if '9' in option_str:
+            return #(IGNORE BORDER CASES FOR NOW)
         if(option_int == 0):
             #no neighbors, move randomly
             move_random(board,cell)
     elif(isinstance(cell, Prey)):
         #update cell based on option
+        if '9' in option_str:
+            return #(IGNORE BORDER CASES FOR NOW)
         if(option_int == 0):
             #no neighbors, move randomly
             move_random(board,cell)
@@ -201,4 +205,9 @@ def update_board_state(board):
             if cell != None:
                 option = get_neighborhood_option(board, cell)
                 update_cell(board,cell,option)
+    #reset cell movement flag
+    for row in board:
+        for cell in row:
+            if cell != None:
+                cell.move = True
     return
